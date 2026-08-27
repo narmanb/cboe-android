efdfa64ec32df5539de31c00f942ea2b86c21234
narmanb <43427228+narmanb@users.noreply.github.com>
2026-08-26 23:40:38 -0500

    Apply Android mobile UI v3 stabilization pass

--- android/app/src/main/cpp/android_project_include_v2.cmake
+++ android/app/src/main/cpp/android_project_include_v2.cmake
@@ -3,3 +3,4 @@
 # Android layout/input fixes once to OpenBoE's source before it is compiled.
 include("${CMAKE_CURRENT_LIST_DIR}/android_project_include.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/android_mobile_ui_fixes.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/android_mobile_ui_fixes_v3.cmake")
