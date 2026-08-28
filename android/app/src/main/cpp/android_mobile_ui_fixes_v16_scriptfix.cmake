fcc244007ff35db007ed061fcf81561388a8cf05
narmanb <43427228+narmanb@users.noreply.github.com>
2026-08-27 23:02:32 -0500

    Fix v16 CMake anchor quoting

--- android/app/src/main/cpp/android_mobile_ui_fixes_v16_scriptfix.cmake
+++ android/app/src/main/cpp/android_mobile_ui_fixes_v16_scriptfix.cmake
@@ -9,7 +9,9 @@ set(CBOE_ANDROID_MOBILE_UI_FIXES_V16_SCRIPTFIX_APPLIED TRUE CACHE INTERNAL "" FO
 set(CBOE_ANDROID_V16_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/android_mobile_ui_fixes_v16.cmake")
 file(READ "${CBOE_ANDROID_V16_SCRIPT}" V16_SCRIPT)
 
-set(V16_SCRIPT_OLD [=[set(V16_MOUSE_MOVE_OLD [=[            case sf::Event::MouseMoved: {
+# Use a higher bracket delimiter for these outer string literals because the
+# text being edited itself contains CMake [=[ ... ]=] literals.
+set(V16_SCRIPT_OLD [==[set(V16_MOUSE_MOVE_OLD [=[            case sf::Event::MouseMoved: {
                 const int x = event.mouseMove.x;
                 const int y = event.mouseMove.y;
                 if(&win == &mainPtr() && android_info_panel_contains(x, y)) {
@@ -27,15 +29,15 @@ set(V16_MOUSE_MOVE_NEW [=[            case sf::Event::MouseMoved: {
                 if(&win == &mainPtr() && android_info_panel_contains(x, y)) {
                     event.type = sf::Event::Count;
                     break;
-                }]=])]=])
+                }]=])]==])
 
-set(V16_SCRIPT_NEW [=[set(V16_MOUSE_MOVE_OLD [=[            case sf::Event::MouseMoved: {]=])
+set(V16_SCRIPT_NEW [==[set(V16_MOUSE_MOVE_OLD [=[            case sf::Event::MouseMoved: {]=])
 set(V16_MOUSE_MOVE_NEW [=[            case sf::Event::MouseMoved: {
                 if(&win == &mainPtr() && android_panel_scroll_target != ANDROID_PANEL_SCROLL_NONE) {
                     android_update_panel_scroll(event.mouseMove.x, event.mouseMove.y);
                     event.type = sf::Event::Count;
                     break;
-                }]=])]=])
+                }]=])]==])
 
 string(FIND "${V16_SCRIPT}" "${V16_SCRIPT_OLD}" V16_SCRIPT_POS)
 if(V16_SCRIPT_POS EQUAL -1)
